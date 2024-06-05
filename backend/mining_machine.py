7e4560abae7b21887f8075b4879caa0c3dd011c7
Emanuele Quattrocchi <emanuele.quattrocchi@itsincom.it>
2024-06-05 12:52:21 +0200

    resolved conflicts

--- backend/mining_machine.py
+++ backend/mining_machine.py
@@ -8,18 +8,20 @@ class MiningMachine:
     mac: str
     ActiveWorker: str
     temps: list
-    hash_rates: list
 
-    def __init__(self, ip_address: str, hostname: str, mac: str, temps: list, hash_rates: list) -> None:
+    def __init__(self,hostname: str,
+    ip_address: str,
+    SerialNumber: str,
+    hash_rates: list,
+    mac: str,
+    ActiveWorker: str,
+    temps: list,) -> None:
         self.ip_address = ip_address
-        self.SerialNumber = SerialNumber
+        self.SerialNumber = ""
         self.hash_rates = hash_rates
         self.mac = mac
-        self.ActiveWorker = ActiveWorker
+        self.ActiveWorker = ""
         self.temps = temps
-        self.hash_rates = hash_rates
-        self.serial_number = ""
-        self.active_worker = ""
 
     def toJSON(self):
         return json.dumps(
